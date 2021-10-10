d8bfce5b437e0342bcecee417a0122e7ce90a53c
Xenon <jupadhyay02@gmail.com>
2021-10-10 19:39:54 +0530

    Introduced some preliminary move generation helper functions. Build failing ....

--- board.h
+++ board.h
@@ -5,24 +5,50 @@
 #include <string>
 #include <unordered_map>
 #include "pieces.h"
+#define CLASSICAL_BITBOARD_BLACK 0b\
+11111111\
+11111111\
+00000000\
+00000000\
+00000000\
+00000000\
+00000000\
+00000000
+
+#define CLASSICAL_BITBOARD_WHITE 0b\
+00000000\
+00000000\
+00000000\
+00000000\
+00000000\
+00000000\
+11111111\
+11111111
 
 class Board{
 private:
     static const int b_size = 8;
-    std::unordered_map<int , Piece> b_pieces;
+    Bitboard b_bitboard_white;
+    Bitboard b_bitboard_black;
+    std::unordered_map<LocationKey , Piece*> b_pieces;   //Change to std::map and see the changes in efficiency.
     bool b_whiteToMove;
+    std::pair<LocationKey , LocationKey> b_kings;
 public:
-    static Location indexToLocation(int);
-    static int locationToIndex(Location);
+    ~Board(){
+        for (auto piece: b_pieces) delete piece.second;
+    }
     static bool getTileColor(Location);
+    static Piece *defaultPiece(LocationKey) ;
 
-    void setPiece(Piece*, int);
-    Piece* getPieceByLocation(Location);
     explicit Board(int size = 8);
     explicit Board(const std::string&);
+
+    void setPiece(Piece*, LocationKey);
+    Piece* getPieceByLocation(LocationKey);
+    void printTile(LocationKey);
     void printBoard();
-    static Piece *defaultPiece(int) ;
-    void printTile(Location);
+
+    void getMovesForPiece(LocationKey);
 
 };
 #endif
